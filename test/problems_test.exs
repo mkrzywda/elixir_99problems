6ee938f9465c7dc497bed04c7b47bc50a2e4c481
mkrzywda <43749180+mkrzywda@users.noreply.github.com>
2020-02-17 07:54:44 +0100

    Add test for p2

--- test/problems_test.exs
+++ test/problems_test.exs
@@ -4,7 +4,14 @@ defmodule PROBLEMSTest do
   
   test "PROBLEMS.last" do
     assert PROBLEMS.last([]) == nil
-    assert PROBLEMS.last([1]) == 1
-    assert PROBLEMS.last([1, 2, 3, 4]) == 4
+    assert PROBLEMS.last([0]) == 0
+    assert PROBLEMS.last([1, 8, 3, 0]) == 0
+    assert PROBLEMS.last([1, 1, 1, 4]) == 4
+    
   end
+  
+  test "PROBLEMS.last_but_one" do
+    assert PROBLEMS.last_but_one([1, 2, 3]) == 2
+    assert PROBLEMS.last_but_one([1, 2]) == 1
+  
 end
