2bc786065bb7ec82fae3bbbc3b4fa4972ef796e0
mkrzywda <43749180+mkrzywda@users.noreply.github.com>
2020-02-17 07:52:48 +0100

    Fix

--- lib/problems.ex
+++ lib/problems.ex
@@ -1,9 +1,12 @@
 defmodule PROBLEMS do
-
+    
+    #P1
     def last([]), do: nil
     def last([a]), do: a
     def last([_head | tail]), do: last(tail)
     
-      def last_but_one([head, _]), do: head
-      def last_but_one([ _ | tail]), do: last_but_one(tail)
+    # P2
+    def last_but_one([head, _]), do: head
+    def last_but_one([ _ | tail]), do: last_but_one(tail)
+    
 end
