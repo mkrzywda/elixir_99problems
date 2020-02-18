7dc4ca7af83f9a4f64a33d14d029230b50706f82
Maciej Krzywda <43749180+mkrzywda@users.noreply.github.com>
2020-02-18 10:24:30 +0100

    P4

--- lib/problems.ex
+++ lib/problems.ex
@@ -9,7 +9,7 @@ defmodule PROBLEMS do
     def last_but_one([head, _]), do: head
     def last_but_one([ _ | tail]), do: last_but_one(tail)
     
-    #P3 
+    # P3 
     def kth_elem(position, list), do: do_at(position, list, 1)
     
     # private func for P3
@@ -17,4 +17,8 @@ defmodule PROBLEMS do
     defp kth(pos, [head | _], counter) when position == counter, do: head
     defp kth(pos, [_ | tail], counter), do: kth(position, tail, counter + 1)
     
+    
+    # P4
+    def len([]), do: 0
+    def len([_ | tail]), do: 1 + len(tail)
 end
