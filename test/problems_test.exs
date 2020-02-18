46afdcf73f60bbeabea7539aa12a0269de7abb86
Maciej Krzywda <43749180+mkrzywda@users.noreply.github.com>
2020-02-18 10:24:14 +0100

    TDD for reverse problem (P5) and fix bugs.
    forgot 'end' after every test case.

--- test/problems_test.exs
+++ test/problems_test.exs
@@ -6,20 +6,29 @@ defmodule PROBLEMSTest do
     assert PROBLEMS.last([]) == nil
     assert PROBLEMS.last([0]) == 0
     assert PROBLEMS.last([1, 8, 3, 0]) == 0
-    assert PROBLEMS.last([1, 1, 1, 4]) == 4
-    
+    assert PROBLEMS.last([1, 1, 1, 4]) == 4  
   end
   
   test "PROBLEMS.last_but_one" do
     assert PROBLEMS.last_but_one([1, 2, 3]) == 2
     assert PROBLEMS.last_but_one([1, 2]) == 1
+  end
   
   
   test "PROBLEMS.kth_elem" do
     assert PROBLEMS.kth_elem(3, [10, 6, 4]) == 4
     assert PROBLEMS.kth_elem(11, []) == nil
+  end
   
   test "PROBLEMS.length" do
     assert PROBLEMS.len([]) == 0
+    assert PROBLEMS.len([1]) == 1
     assert PROBLEMS.len([1, 2, 3]) == 3
+  end
+  
+  test "PROBLEMS.reverse" do
+    assert PROBLEMS.reverse([1, 2, 3, 4]) == [4, 3, 2, 1]
+    assert PROBLEMS.reverse([1]) == [1]
+    assert PROBLEMS.reverse([]) == []
+  end
 end
