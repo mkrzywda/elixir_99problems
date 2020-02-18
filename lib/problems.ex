17037be45b05f03419f76e68bdb73ff55dd83da1
Maciej Krzywda <43749180+mkrzywda@users.noreply.github.com>
2020-02-18 10:17:40 +0100

    Find the k'th element of a list.

--- lib/problems.ex
+++ lib/problems.ex
@@ -9,4 +9,12 @@ defmodule PROBLEMS do
     def last_but_one([head, _]), do: head
     def last_but_one([ _ | tail]), do: last_but_one(tail)
     
+    #P3 
+    def kth_elem(position, list), do: do_at(position, list, 1)
+    
+    # private func for P3
+    defp kth(_, [], _), do: nil
+    defp kth(pos, [head | _], counter) when position == counter, do: head
+    defp kth(pos, [_ | tail], counter), do: kth(position, tail, counter + 1)
+    
 end
