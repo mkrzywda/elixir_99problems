8e135bb0affcff0e4c448ccccf0bde34cf5f887e
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2020-02-25 22:27:39 +0100

    Fix bugs and add P6

--- lib/problems.ex
+++ lib/problems.ex
@@ -10,12 +10,12 @@ defmodule PROBLEMS do
     def last_but_one([ _ | tail]), do: last_but_one(tail)
     
     # P3 
-    def kth_elem(position, list), do: do_at(position, list, 1)
+    def kth_elem(pos, list), do: kth(pos, list, 1)
     
     # private func for P3
     defp kth(_, [], _), do: nil
-    defp kth(pos, [head | _], counter) when position == counter, do: head
-    defp kth(pos, [_ | tail], counter), do: kth(position, tail, counter + 1)
+    defp kth(pos, [head | _], counter) when pos == counter, do: head
+    defp kth(pos, [_ | tail], counter), do: kth(pos, tail, counter + 1)
     
     # P4
     def len([]), do: 0
@@ -25,4 +25,12 @@ defmodule PROBLEMS do
     def reverse(list), do: do_reverse(list, [])
     defp do_reverse([], acc), do: acc
     defp do_reverse([head | tail], acc), do: do_reverse(tail, [head | acc])
+
+    # P6
+    def palindrome(list), do: is_palindrome([],list)
+    defp is_palindrome(left, right) when length(left) == length(right), do: left == right
+    defp is_palindrome(left, [_|right]) when length(left) == length(right), do: left == right
+    defp is_palindrome(left, right) when length(left) == length(right), do: left == right
+
+    
 end
